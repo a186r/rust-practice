75fd767c0cca060915735e91328122bed32fafed
a186r <hi.aewalle@gmail.com>
2020-03-27 18:15:11 +0800

    feat(wasm): 完成康威生命游戏

--- wasm/wasm-game-of-life/src/lib.rs
+++ wasm/wasm-game-of-life/src/lib.rs
@@ -1,5 +1,7 @@
 mod utils;
 
+use std::fmt;
+use wasm_bindgen::__rt::core::fmt::{Error, Formatter};
 use wasm_bindgen::prelude::*;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -9,11 +11,125 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern "C" {
-    fn alert(s: &str);
+//每个单元格被表示为单个字节
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Cell {
+    Dead = 0,
+    Alive = 1,
 }
 
+//定义宇宙
 #[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
+pub struct Universe {
+    width: u32,
+    height: u32,
+    cells: Vec<Cell>,
+}
+
+//公共函数，暴露给javascript
+#[wasm_bindgen]
+impl Universe {
+    pub fn tick(&mut self) {
+        let mut next = self.cells.clone();
+
+        for row in 0..self.height {
+            for col in 0..self.width {
+                let idx = self.get_index(row, col);
+                let cell = self.cells[idx];
+                let live_neighbors = self.live_neighbor_count(row, col);
+
+                let next_cell = match (cell, live_neighbors) {
+                    //    规则1，任何存活细胞周围少于2个存活邻居则死亡，仿佛是人口过少造成的
+                    (Cell::Alive, x) if x < 2 => Cell::Dead,
+                    //    规则2，任何存活细胞周围有2个或者3个存活邻居则活到下一代
+                    (Cell::Alive, 2) | (Cell::Alive, 3) => Cell::Alive,
+                    //规则3，任何存活细胞周围有超过3个存活邻居则死亡，仿佛是人口过剩造成的
+                    (Cell::Alive, x) if x > 3 => Cell::Dead,
+                    //    规则4，任何死亡细胞周围有正好3个存活细胞则变成存活细胞，仿佛是生殖
+                    (Cell::Dead, 3) => Cell::Alive,
+                    //    其他所有单元格保持同样的状态
+                    (otherwise, _) => otherwise,
+                };
+
+                next[idx] = next_cell;
+            }
+        }
+        self.cells = next;
+    }
+
+    //为了访问给定行列的单元格，将行列翻译为单元格向量的索引
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    //为了计算单元格的下一个状态，需要得到一个有多少邻居存活的计数
+    fn live_neighbor_count(&self, row: u32, column: u32) -> u8 {
+        let mut count = 0;
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                count += self.cells[idx] as u8;
+            }
+        }
+        count
+    }
+
+    //构造函数
+    pub fn new() -> Universe {
+        let width = 64;
+        let height = 64;
+
+        let cells = (0..width * height)
+            .map(|i| {
+                if i % 2 == 0 || i % 7 == 0 {
+                    Cell::Alive
+                } else {
+                    Cell::Dead
+                }
+            })
+            .collect();
+
+        Universe {
+            width,
+            height,
+            cells,
+        }
+    }
+
+    pub fn render(&self) -> String {
+        self.to_string()
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
+}
+
+impl fmt::Display for Universe {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.cells.as_slice().chunks(self.width as usize) {
+            for &cell in line {
+                let symbol = if cell == Cell::Dead { '◻' } else { '◼' };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
 }
--- wasm/wasm-game-of-life/www/index.js
+++ wasm/wasm-game-of-life/www/index.js
@@ -1,3 +1,79 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe, Cell } from "wasm-game-of-life";
+import {memory} from "wasm-game-of-life/wasm_game_of_life_bg"; // 导入WebAssembly memory
 
-wasm.greet("a186r");
+const CELL_SIZE = 5;
+const GRID_COLOR = "#CCCCCC";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+
+//构建宇宙，并获取其宽高
+const universe = Universe.new();
+const width = universe.width();
+const height = universe.height();
+
+//给细胞画布一个1像素的边框
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+const ctx = canvas.getContext('2d');
+
+const renderLoop = () => {
+    universe.tick();
+
+    drawGrid();
+    drawCells();
+
+    requestAnimationFrame(renderLoop);
+};
+
+// requestAnimationFrame(renderLoop);
+
+//为了绘制细胞之间的网格，我们绘制一组等间隔的水平线和一组等间隔的垂直线，形成网格
+const drawGrid = () => {
+    ctx.beginPath();
+    ctx.strokeStyle = GRID_COLOR;
+
+//    垂直线
+    for (let i = 0; i <= width; i++) {
+        ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+        ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+    }
+//    水平线
+    for (let j = 0; j <= height; j++) {
+        ctx.moveTo(0, j*(CELL_SIZE + 1) * height + 1);
+        ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+    }
+    ctx.stroke();
+};
+
+const getIndex = (row, colum) => {
+    return row * width + colum;
+};
+
+const drawCells = () => {
+    const cellsPtr = universe.cells();
+    const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+    ctx.beginPath();
+
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            ctx.fillStyle = cells[idx] === Cell.Dead
+                ?DEAD_COLOR
+                :ALIVE_COLOR;
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+    ctx.stroke();
+};
+
+drawGrid();
+drawCells();
+requestAnimationFrame(renderLoop);
