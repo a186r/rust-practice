02458cafa882dec2e630ecb984c3323f01d39fd9
a186r <hi.aewalle@gmail.com>
2020-03-31 16:29:11 +0800

    feat(index.js): 使用window.performance.now函数创建一个帧率计时器

--- wasm/wasm-game-of-life/www/index.js
+++ wasm/wasm-game-of-life/www/index.js
@@ -66,10 +66,12 @@ const drawCells = () => {
 let animationId = null;
 
 const renderLoop = () => {
+    fps.render();
+    universe.tick();
+
     drawGrid();
     drawCells();
 
-    universe.tick();
 
     animationId = requestAnimationFrame(renderLoop);
 };
@@ -118,3 +120,45 @@ canvas.addEventListener("click",  event => {
     drawGrid();
     drawCells();
 });
+
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+    //    将上一次渲染帧的事件增量转换为fps的测量
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+    //    只保存最新的100个时间
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+    //    找出100个最近事件的最大值、最小值和平均值
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for(let i = 0; i < this.frames.length; i++){
+            sum += this.frames[i];
+            min = Math.min(this.frames[i], min);
+            max = Math.max(this.frames[i], max);
+        }
+        let mean = sum / this.frames.length;
+
+    //    渲染统计信息
+        this.fps.textContent = `
+Frames per Second:
+         latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+};
